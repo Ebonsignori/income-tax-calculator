17220dc79c1dd50c8a37e83cb212d7a022863ccd
Evan Bonsignori <evanabonsignori@gmail.com>
2024-02-07 22:42:21 -0800

    lint fix

--- src/app/[year]/[state]/[city]/not-found.tsx
+++ src/app/[year]/[state]/[city]/not-found.tsx
@@ -1,7 +1,7 @@
 import { NotFoundPage } from "@/components/NotFound";
 import Wrapper from "@/components/Wrapper";
 
-export default function NotFound(props) {
+export default function NotFound() {
   return (
     <Wrapper title="Page Not Found">
       <NotFoundPage />
